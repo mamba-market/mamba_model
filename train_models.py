276daf4e1e156a3018ead81be50362a7357ab6cf
Tianlong Xu <txu0915@gmail.com>
2023-10-18 18:35:02 -0400

    add data sampling mechanism

--- train_models.py
+++ train_models.py
@@ -34,39 +34,52 @@ def main(cfg: DictConfig):
         data = pandas.read_csv(input_fp)
     else:
         data = pandas.read_pickle(input_fp)
+    # stratified sampling weights
+    logging.info(f"Data size before filtering {len(data)}")
+    for minority_sampling_category in cfg.minority_sampling_categories:
+        data['freq'] = data.groupby(minority_sampling_category)[cfg.response].transform('count')
+        # eliminating extremely under represented data
+        data = data[data['freq'] >= cfg.minority_sample_limit].copy()
+    logging.info(f"Data size after removing under represented rows {len(data)}.")
+    if cfg.training_sample_size is not None:
+        data = data.sample(n=cfg.training_sample_size, replace=False,
+                           weights=data.groupby(list(cfg.stratefied_sampling_categories))[cfg.response].transform('count'))
+    logging.info(f"Data size after stratified sampling {len(data)}")
+    logging.info(f"{data.describe()}")
     data = shuffle(data)
+    data.reset_index(inplace=True, drop=True)
     for col in cfg.categorical_features:
         le = LabelEncoder()
         data[col] = le.fit_transform(data[col])
+        logging.info(f"Converting feature {col} into {len(le.classes_)} levels.")
     dims_categorical_vars = [data[col].max() + 1 for col in cfg.categorical_features]
     dim_numerical_vars = len(cfg.numerical_features)
+    logging.info(f"Total dims of categorical vars: \n{dims_categorical_vars}")
+    logging.info(f"Dim of numerical vars: \n{dim_numerical_vars}")
+    logging.info(f"Range of target value: {data[cfg.response].min(), data[cfg.response].max()}")
 
-    # splitting data
-    train_df = data.sample(frac=0.8, random_state=42)
-    test_df = data.drop(train_df.index)
-    train_df.reset_index(inplace=True, drop=True)
-    test_df.reset_index(inplace=True, drop=True)
-    logging.info("Sizes of training and testing datasets")
-    logging.info(f"Training: {len(train_df)}")
-    logging.info(f"Testing: {len(test_df)}")
-
-    # composing dataset
-    train_dataset = CriketScoreDataSetWithCatAndNum(train_df, cfg.categorical_features, cfg.numerical_features,
-                                                    cfg.response)
-    test_dataset = CriketScoreDataSetWithCatAndNum(test_df, cfg.categorical_features, cfg.numerical_features,
-                                                   cfg.response)
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size)
-    logging.info(f"Data loader assembled.")
     # model initialization
     model = FactorizationMachine(cat_dims=dims_categorical_vars, num_dim=dim_numerical_vars,
-                                 k=10, attention_dim=50).to(device)
-    criterion = torch.nn.MSELoss()
+                                 k=cfg.embedding_dim, attention_dim=cfg.attention_dim).to(device)
+    criterion = torch.nn.L1Loss()
     optimizer = torch.optim.Adam(params=model.parameters(), lr=learning_rate, weight_decay=weight_decay)
     early_stopper = EarlyStopping(patience=3)
     logging.info("Model successfully initialized.")
 
     train_losses, test_losses = [], []
+    # splitting data, still following the stratified rule.
+
+    train_loss, test_loss = -10, 10
+    test_loader, train_loader = forming_train_and_test_data(batch_size, cfg, data)
+    while abs(train_loss - test_loss) > cfg.allowed_initial_loss_diff:
+        logging.info(f"Regenerating test and train data to offset initial loss diff {abs(train_loss - test_loss)}...")
+        test_loader, train_loader = forming_train_and_test_data(batch_size, cfg, data)
+        # pretraining loss
+        train_loss, test_loss = evaluate(model, train_loader, criterion, device), \
+                                evaluate(model, test_loader, criterion, device)
+    train_losses.append(train_loss)
+    test_losses.append(test_loss)
+    logging.info(f"Pretraining losses for train and test dataset are {train_loss}, {test_loss}, respectively.")
     for epoch in range(epochs):
         train_loss = train(model, train_loader, optimizer, criterion, device)
         test_loss = evaluate(model, test_loader, criterion, device)
@@ -80,6 +93,32 @@ def main(cfg: DictConfig):
 
     torch.save(model.state_dict(), os.path.join(cfg.model_fp, cfg.best_model_name))
     plot_losses(train_losses, test_losses, save_path=cfg.loss_plot_fp)
+
+
+def forming_train_and_test_data(batch_size, cfg, data):
+    train_df = data.sample(frac=0.8,
+                           weights=data.groupby(list(cfg.stratefied_sampling_categories))[cfg.response].transform(
+                               'count'))
+    test_df = data.drop(train_df.index).copy()
+    train_df.reset_index(inplace=True, drop=True)
+    test_df.reset_index(inplace=True, drop=True)
+    logging.info("Sizes of training and testing datasets")
+    logging.info(f"Training: {len(train_df)} \n {train_df.groupby(list(cfg.stratefied_sampling_categories)).size()}")
+    logging.info(f"Testing: {len(test_df)} \n {test_df.groupby(list(cfg.stratefied_sampling_categories)).size()}")
+    for stratefied_sampling_category in cfg.stratefied_sampling_categories:
+        logging.info(f"Training data value counts on {stratefied_sampling_category}, "
+                     f"{train_df[stratefied_sampling_category].value_counts()}")
+        logging.info(f"Testing data value counts on {stratefied_sampling_category}, "
+                     f"{test_df[stratefied_sampling_category].value_counts()}")
+    # composing dataset
+    train_dataset = CriketScoreDataSetWithCatAndNum(train_df, cfg.categorical_features, cfg.numerical_features,
+                                                    cfg.response)
+    test_dataset = CriketScoreDataSetWithCatAndNum(test_df, cfg.categorical_features, cfg.numerical_features,
+                                                   cfg.response)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size)
+    logging.info(f"Data loader assembled.")
+    return test_loader, train_loader
 
 
 if __name__ == '__main__':
